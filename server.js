3b1425f3102404a7d6da211f4c45af5498c8ff5b
SondreB <pixelworld@hotmail.com>
2016-03-14 15:10:21 +0100

    Setup for IIS on Azure

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
+
 var express = require('express');
 var app = express();
 app.use(express.static('public'));
 var http = require('http').Server(app);
-var port = 3000;
+var port = process.env.PORT || 8080;
 
 http.listen(port, function() {
     console.log('listening on *: ' + port);
